255e64090236cf57ec917602eb74692d5b418050
raghavyadavm <raghavyadav258@gmail.com>
2018-05-08 12:50:16 -0500

    cmms data

--- public/js/bems.js
+++ public/js/bems.js
@@ -74,3 +74,54 @@ function handleIFCFile(input) {
 
   reader.readAsText(file);
 }
+
+document.getElementById('cmmsControlFile').addEventListener('change', handleCmmsFile, false);
+var rABS = true;
+var alarmList = [];
+
+function handleCmmsFile(e) {
+  f = e.target.files[0];
+  var reader = new FileReader();
+  reader.onload = function (e) {
+    var data = e.target.result;
+    if (!rABS) data = new Uint8Array(data);
+    var workbook = XLSX.read(data, {
+      type: rABS ? 'binary' : 'array'
+    });
+    worksheet = workbook.Sheets["Sheet1"];
+    // console.log(worksheet);
+    var jsonData = XLSX.utils.sheet_to_json(worksheet);
+    // console.log(jsonData);
+
+    for (const key in jsonData) {
+      if (jsonData.hasOwnProperty(key)) {
+        for (const innerkey in jsonData[key]) {
+          if (jsonData[key].hasOwnProperty(innerkey)) {
+            // console.log(((jsonData[key])[innerkey]));
+            alarmList.forEach(element => {
+              if (((jsonData[key])[innerkey]).includes(element)) {
+                var s = (jsonData[key])[innerkey];
+                console.log(s);
+                console.log(jsonData[key]);
+                document.getElementById("maintainance-div").children[1].innerHTML += "<li class='list-group-item'>" + 'CMMS ID' + " : " + jsonData[key]['CMMS ID'] + "</li>";
+                document.getElementById("maintainance-div").children[1].innerHTML += "<li class='list-group-item'>" + 'Location' + " : " + jsonData[key]['Room'] + "</li>";                
+                document.getElementById("maintainance-div").children[1].innerHTML += "<li class='list-group-item'>" + 'Maintenance Type' + " : " + jsonData[key]['Maintenance Type'] + "</li>";
+                document.getElementById("maintainance-div").children[1].innerHTML += "<li class='list-group-item'>" + 'Manufacturer' + " : " + jsonData[key]['Mfr'] + "</li>";                
+                document.getElementById("maintainance-div").children[1].innerHTML += "<li class='list-group-item'>" + 'Model number' + " : " + jsonData[key]['Model number'] + "</li>";
+                document.getElementById("maintainance-div").children[1].innerHTML += "<li class='list-group-item'>" + 'Serial No' + " : " + jsonData[key]['Serial No'] + "</li>";
+                document.getElementById("maintainance-div").children[1].innerHTML += "<li class='list-group-item'>" + 'Serves' + " : " + jsonData[key]['Serves'] + "</li>";
+                document.getElementById("maintainance-div").children[1].innerHTML += "<li class='list-group-item'>" + 'Warranty Date' + " : " + jsonData[key]['Warranty Date'] + "</li>";
+                document.getElementById("maintainance-div").children[1].innerHTML += "<li class='list-group-item'>" + 'Maintenance cost' + " : " + jsonData[key]['Total Costs/WO'] + "</li>";
+                // document.getElementById("maintainance-div").children[1].innerHTML += "<li class='list-group-item'>" + x + " : " + (jsonData[key])[x] + "</li>";
+                // document.getElementById("maintainance-div").children[1].innerHTML += "<li class='list-group-item'>" + s + "</li>";
+              }
+            });
+          }
+        }
+      }
+    }
+  };
+
+  if (rABS) reader.readAsBinaryString(f);
+  else reader.readAsArrayBuffer(f);
+}
