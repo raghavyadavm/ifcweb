0c55bb59bb80d7e4fc14b5f787e1544456df7873
Raghavender Mylagary <raghavyadav258@gmail.com>
2018-05-13 23:21:33 -0500

    added maintainance list

--- public/js/bems.js
+++ public/js/bems.js
@@ -1,6 +1,7 @@
 document.getElementById('bemsControlFile').addEventListener('change', handleBemsFile, false);
 var rABS = true;
 var alarmList = [];
+var maintainanceList = [];
 
 function handleBemsFile(e) {
   f = e.target.files[0];
@@ -36,48 +37,8 @@ function handleBemsFile(e) {
   else reader.readAsArrayBuffer(f);
 }
 
-document.getElementById('ifcControlFile').addEventListener('change', handleIFCFile, false);
-var allLines;
-var data;
-var info, endPosition = 0,
-  startPosition = 0,
-  sillyString = "";
-
-function handleIFCFile(input) {
-  info = input;
-  const file = input.target.files[0];
-  const reader = new FileReader();
-
-  reader.onload = (event) => {
-    data = event.target.result;
-    allLines = data.split("\n");
-    // Reading line by line
-    allLines.map((line) => {
-      if (line != "" && line.startsWith("#")) {
-        if (line.includes("BEMS ID")) {
-          alarmList.forEach(element => {
-            if (line.includes(element)) {
-              console.warn(line);
-            }
-          });
-        }
-      }
-      // if (line.includes("CMMS ID")) {
-      //   console.log(line);
-      // }
-    });
-  };
-
-  reader.onerror = (evt) => {
-    alert(evt.target.error.name);
-  };
-
-  reader.readAsText(file);
-}
-
 document.getElementById('cmmsControlFile').addEventListener('change', handleCmmsFile, false);
 var rABS = true;
-var alarmList = [];
 
 function handleCmmsFile(e) {
   f = e.target.files[0];
@@ -103,17 +64,16 @@ function handleCmmsFile(e) {
                 var s = (jsonData[key])[innerkey];
                 console.log(s);
                 console.log(jsonData[key]);
-                document.getElementById("maintainance-div").children[1].innerHTML += "<li class='list-group-item'>" + 'CMMS ID' + " : " + jsonData[key]['CMMS ID'] + "</li>";
-                document.getElementById("maintainance-div").children[1].innerHTML += "<li class='list-group-item'>" + 'Location' + " : " + jsonData[key]['Room'] + "</li>";                
-                document.getElementById("maintainance-div").children[1].innerHTML += "<li class='list-group-item'>" + 'Maintenance Type' + " : " + jsonData[key]['Maintenance Type'] + "</li>";
-                document.getElementById("maintainance-div").children[1].innerHTML += "<li class='list-group-item'>" + 'Manufacturer' + " : " + jsonData[key]['Mfr'] + "</li>";                
-                document.getElementById("maintainance-div").children[1].innerHTML += "<li class='list-group-item'>" + 'Model number' + " : " + jsonData[key]['Model number'] + "</li>";
-                document.getElementById("maintainance-div").children[1].innerHTML += "<li class='list-group-item'>" + 'Serial No' + " : " + jsonData[key]['Serial No'] + "</li>";
-                document.getElementById("maintainance-div").children[1].innerHTML += "<li class='list-group-item'>" + 'Serves' + " : " + jsonData[key]['Serves'] + "</li>";
-                document.getElementById("maintainance-div").children[1].innerHTML += "<li class='list-group-item'>" + 'Warranty Date' + " : " + jsonData[key]['Warranty Date'] + "</li>";
-                document.getElementById("maintainance-div").children[1].innerHTML += "<li class='list-group-item'>" + 'Maintenance cost' + " : " + jsonData[key]['Total Costs/WO'] + "</li>";
-                // document.getElementById("maintainance-div").children[1].innerHTML += "<li class='list-group-item'>" + x + " : " + (jsonData[key])[x] + "</li>";
-                // document.getElementById("maintainance-div").children[1].innerHTML += "<li class='list-group-item'>" + s + "</li>";
+                maintainanceList.push(jsonData[key]);
+                // document.getElementById("maintainance-div").children[1].innerHTML += "<li class='list-group-item'>" + 'CMMS ID' + " : " + jsonData[key]['CMMS ID'] + "</li>";
+                // document.getElementById("maintainance-div").children[1].innerHTML += "<li class='list-group-item'>" + 'Location' + " : " + jsonData[key]['Room'] + "</li>";                
+                // document.getElementById("maintainance-div").children[1].innerHTML += "<li class='list-group-item'>" + 'Maintenance Type' + " : " + jsonData[key]['Maintenance Type'] + "</li>";
+                // document.getElementById("maintainance-div").children[1].innerHTML += "<li class='list-group-item'>" + 'Manufacturer' + " : " + jsonData[key]['Mfr'] + "</li>";                
+                // document.getElementById("maintainance-div").children[1].innerHTML += "<li class='list-group-item'>" + 'Model number' + " : " + jsonData[key]['Model number'] + "</li>";
+                // document.getElementById("maintainance-div").children[1].innerHTML += "<li class='list-group-item'>" + 'Serial No' + " : " + jsonData[key]['Serial No'] + "</li>";
+                // document.getElementById("maintainance-div").children[1].innerHTML += "<li class='list-group-item'>" + 'Serves' + " : " + jsonData[key]['Serves'] + "</li>";
+                // document.getElementById("maintainance-div").children[1].innerHTML += "<li class='list-group-item'>" + 'Warranty Date' + " : " + jsonData[key]['Warranty Date'] + "</li>";
+                // document.getElementById("maintainance-div").children[1].innerHTML += "<li class='list-group-item'>" + 'Maintenance cost' + " : " + jsonData[key]['Total Costs/WO'] + "</li>";
               }
             });
           }
@@ -124,4 +84,43 @@ function handleCmmsFile(e) {
 
   if (rABS) reader.readAsBinaryString(f);
   else reader.readAsArrayBuffer(f);
+}
+
+document.getElementById('ifcControlFile').addEventListener('change', handleIFCFile, false);
+var allLines;
+var data;
+var info, endPosition = 0,
+  startPosition = 0,
+  sillyString = "";
+
+function handleIFCFile(input) {
+  info = input;
+  const file = input.target.files[0];
+  const reader = new FileReader();
+
+  reader.onload = (event) => {
+    data = event.target.result;
+    allLines = data.split("\n");
+    // Reading line by line
+    allLines.map((line) => {
+      if (line != "" && line.startsWith("#")) {
+        if (line.includes("CMMS ID")) {
+          alarmList.forEach(element => {
+            if (line.includes(element)) {
+              console.warn(line);
+            }
+          });
+        }
+      }
+      // if (line.includes("CMMS ID")) {
+      //   console.log(line);
+      // }
+    });
+  };
+
+  reader.onerror = (evt) => {
+    alert(evt.target.error.name);
+  };
+
+  reader.readAsText(file);
 }
